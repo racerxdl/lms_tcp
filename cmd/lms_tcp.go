26f8ff7a8b200cf7575035de60cfcd8f42afc9c2
Lucas Teske <lucas@teske.net.br>
2021-02-24 22:36:08 -0300

    Optimized the reception

--- cmd/lms_tcp.go
+++ cmd/lms_tcp.go
@@ -5,8 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/myriadrf/limedrv"
+	"github.com/quan-to/slog"
 	"github.com/racerxdl/lms_tcp/rtltcp"
-	"log"
 	"os"
 	"os/signal"
 	"strings"
@@ -16,14 +16,14 @@ import (
 var listenAddress = flag.String("a", "", "listen address")
 var listenPort = flag.Int("p", 1234, "listen port (default: 1234)")
 var gain = flag.Int("g", 0, "gain (default: 0 for auto)")
-var sampleRate = flag.Int("s", 2048000, "sample rate in Hz (default: 2048000 Hz)")
+var sampleRate = flag.Int("s", 4000000, "sample rate in Hz (default: 2048000 Hz)")
 var maxBuffers = flag.Int("b", -1, "number of buffers (kept for compatibility, ignored here)")
 var deviceIndex = flag.Int("d", 0, "device index (default: 0)")
-var oversampling = flag.Int("ov", 16, "oversampling (default: 8)")
+var oversampling = flag.Int("ov", 0, "oversampling (default: 0, maximum possible)")
 var antennaName = flag.String("antenna", "LNAL", "antenna name (default: LNAL)")
 var channel = flag.Int("channel", 0, "channel number (default: 0)")
-var lpf = flag.Int("lpf", 3000000, "low pass filter (default 2500000)")
-
+var lpf = flag.Int("lpf", 2500000, "low pass filter (default 2500000)")
+var verbose = flag.Bool("v", false, "verbose mode (default: off)")
 var server *rtltcp.Server
 
 var tunerValues []int
@@ -42,6 +42,10 @@ func OnSamples16(samples []int16, _ int, _ uint64) {
 
 func main() {
 	flag.Parse()
+	log := slog.Scope("LMSTCP")
+	slog.SetDebug(*verbose)
+	slog.SetShowLines(false)
+
 	addr := fmt.Sprintf("%s:%d", *listenAddress, *listenPort)
 	if *maxBuffers != -1 {
 		fmt.Println("WARNING: number of buffers ignored on lms_tcp")
@@ -69,9 +73,9 @@ func main() {
 	dev.SetSampleRate(float64(*sampleRate), *oversampling)
 
 	if len(dev.RXChannels) <= *channel {
-		fmt.Printf("Invalid channel: %d. Found channels: \n", *channel)
+		log.Error("Invalid channel: %d. Found channels: ", *channel)
 		for i, v := range dev.RXChannels {
-			fmt.Printf("	%d: %s\n", i, strings.ReplaceAll(v.String(), "\n", "\n\t"))
+			log.Error("	%d: %s", i, strings.ReplaceAll(v.String(), "\n", "\n\t"))
 		}
 		return
 	}
@@ -80,7 +84,7 @@ func main() {
 
 	ch.Enable().
 		SetAntennaByName(*antennaName).
-		SetGainDB(uint(*gain)).
+		SetGainNormalized(1).
 		SetLPF(float64(*lpf)).
 		EnableLPF().
 		SetCenterFrequency(106.3e6)
@@ -104,7 +108,7 @@ func main() {
 		switch cmd.Type {
 		case rtltcp.SetSampleRate:
 			sampleRate := binary.BigEndian.Uint32(cmd.Param[:])
-			fmt.Printf("Setting sample rate to %d\n", sampleRate)
+			log.Info("Setting sample rate to %d with oversampling factor %d", sampleRate, *oversampling)
 			dev.Stop()
 			dev.SetSampleRate(float64(sampleRate), *oversampling)
 			dev.Start()
@@ -114,54 +118,45 @@ func main() {
 		case rtltcp.SetGain:
 			gain := binary.BigEndian.Uint32(cmd.Param[:])
 			gainU := uint(gain / 4)
-			fmt.Printf("Setting gain to %d\n", gainU)
+			log.Info("Setting gain to %d", gainU)
 			dev.SetGainDB(*channel, true, gainU)
 		case rtltcp.SetGainMode:
 		case rtltcp.SetTunerGainByIndex:
 			gainIdx := binary.BigEndian.Uint32(cmd.Param[:])
 			if uint32(len(tunerValues)) > gainIdx {
 				gain := tunerValues[gainIdx]
-				fmt.Printf("Setting gain to %d (idx %d)\n", gain, gainIdx)
+				log.Info("Setting gain to %d (idx %d)", gain, gainIdx)
 				dev.SetGainDB(*channel, true, uint(gain))
 			} else {
-				fmt.Printf("Received gain index: %d but that's invalid. maximum is %d\n", gainIdx, len(tunerValues))
+				log.Error("Received gain index: %d but that's invalid. maximum is %d", gainIdx, len(tunerValues))
 			}
 		case rtltcp.SetFrequency:
 			frequency := binary.BigEndian.Uint32(cmd.Param[:])
-			fmt.Printf("Setting frequency to %d\n", frequency)
+			log.Info("Setting frequency to %d", frequency)
 			dev.SetCenterFrequency(*channel, true, float64(frequency))
 			return true
 		default:
-			fmt.Printf("Command %s not handled!\n", cmd.Type)
+			log.Debug("Command %s not handled!", cmd.Type)
 			return true
 		}
 
 		return true
 	})
 	server.SetOnConnect(func(sessionId string, address string) {
-		fmt.Printf("New connection from %s [%s]\n", address, sessionId)
+		log.Debug("New connection from %s [%s]", address, sessionId)
 	})
 
 	dev.Start()
 	err := server.Start()
 	if err != nil {
-		fmt.Printf("Error starting rtltcp: %s\n", err)
+		log.Error("Error starting rtltcp: %s", err)
 		return
 	}
+	log.Info("Setting gain to %d", *gain)
+	ch.SetGainDB(uint(*gain))
 	<-done
 	server.Stop()
 	dev.Stop()
 
-	log.Println("Closed!")
+	log.Info("Closed!")
 }
-
-/*
-   -a listen address
-   -p listen port (default: 1234)
-   -f frequency to tune to [Hz]
-   -g gain (default: 0 for auto)
-   -s samplerate in Hz (default: 2048000 Hz)
-   -b number of buffers (default: 32, set by library)
-   -n max number of linked list buffers to keep (default: 500)
-   -d device_index (default: 0)
-*/
